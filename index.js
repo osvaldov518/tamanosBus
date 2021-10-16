6b3ee1df41d728a13bcebe9c3da7bc9690b2af2d
osvaldov518 <osvaldov518@gmail.com>
2021-10-16 16:34:15 -0500

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,30 @@
+const tamanosBus = async (event) => {
+    // TODO implement
+    
+    var grupos = JSON.parse("["+event.groups+"]"); // capturo el objeto "groups" y lo transformo en un array.
+    
+    var xMax = 0;
+    for (var i = 0; i < grupos.length; i++) {
+        xMax +=grupos[i]; // sumo la cantidad de cada grupo para obtener el x (tamaño del bus), mas grande.
+    }
+    
+    var vSizes = [];
+    
+    var maxGrupo = Math.max.apply(Math, grupos); // obtengo el tamaño del grupo mas grande.
+    
+    for (var j = maxGrupo; j < xMax; j++) { // Inicio el ciclo con el tamaño minimo del bus que siempre será el grupo mas grande.
+        if (xMax%j == 0) { // la operacion modulo me permite obtener los divisores (posibles tamaños del bus), del total de pasajeros.
+            vSizes.push(j);
+        }
+    }
+
+    vSizes.push(xMax); 
+    vSizes = vSizes.join(','); // transformo nuevamente el array a string para poder presentarlo en el Response.
+    
+    const response = {
+        sizes : vSizes
+    };
+    return response;
+};
+
+exports.handler = tamanosBus;
